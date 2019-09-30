d0aeb9353d12b6fb8b0d15f6e44c72414772f651
isLonerism <kiwiclitool@gmail.com>
2019-09-30 22:21:32 +0300

    feat(storage github commit functionality)

--- etc/kiwi/modules/storage.py
+++ etc/kiwi/modules/storage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2
 #kiwidesc=work with common file storage services to store and retrieve files
 import argparse
+import requests
+import base64
+import json
 
 service_args = {
 	'file': [],
@@ -11,10 +14,34 @@ def file(args):
 	with open(args.destination, 'w') as destination_file:
 		destination_file.write(args.content)
 
-# TODO
 def github(args):
-	pass
+	# get remote file
+	response = requests.get('https://{}:{}@api.github.com/repos/{}/{}/contents/{}'.format( \
+                args.auth_user, args.auth_pass, args.repo_owner, args.repo, args.destination)).json()
 
+	# write headers and data
+	headers = {'Content-type': 'application/json'}
+	commit_json = {
+		'message': args.message if args.message else 'No message',
+		'content': base64.b64encode(bytes(args.content), 'utf-8'),
+		'committer': {}
+	}
+
+	# insert optional parameters
+	if args.committer_user:
+		commit_json['committer']['name'] = args.committer_user
+	if args.committer_email:
+                commit_json['committer']['email'] = args.committer_email
+
+	# file blob sha must be present if updating
+	if 'sha' in response:
+		commit_json['sha'] = response['sha']
+
+	# update/create a file
+	requests.put('https://{}:{}@api.github.com/repos/{}/{}/contents/{}'.format( \
+		args.auth_user, args.auth_pass, args.repo_owner, args.repo, args.destination), data=json.dumps(commit_json), headers=headers).json()
+
+# TODO retrieve files functionality
 def kiwi_main():
 	parser = argparse.ArgumentParser(description="work with common file storage services to store and retrieve files")
 
@@ -31,8 +58,10 @@ def kiwi_main():
 	parser.add_argument('-r', '--repo', help='repository name (when using Git)', type=str)
 	parser.add_argument('-o', '--repo-owner', help='repository owner (when using Git)', type=str)
 	parser.add_argument('-m', '--message', help='commit message (when using Git)', type=str)
-	parser.add_argument('-u', '--auth_user', help='authentication username', type=str)
-	parser.add_argument('-p', '--auth_pass', help='authentication password', type=str)
+	parser.add_argument('-u', '--auth-user', help='authentication username', type=str)
+	parser.add_argument('-p', '--auth-pass', help='authentication password', type=str)
+	parser.add_argument('-n', '--committer-user', help='name of the committer (when using Git)', type=str)
+	parser.add_argument('-e', '--committer-email', help='e-mail of the committer (when using Git)', type=str)
 
 	args = parser.parse_args()
 
