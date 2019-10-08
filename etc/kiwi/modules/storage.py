ae984ae23bccd6295aeb39706edbbdc125e47c38
isLonerism <kiwiclitool@gmail.com>
2019-10-08 18:23:34 +0300

    feat(storage module source files)

--- etc/kiwi/modules/storage.py
+++ etc/kiwi/modules/storage.py
@@ -4,15 +4,20 @@ import argparse
 import requests
 import base64
 import json
+import os
 
 service_args = {
 	'file': [],
 	'github': ['repo', 'repo_owner', 'auth_user', 'auth_pass']
 }
 
+# ---------------------------------------------------------------------------------------------------
+
 def file_store(args):
 	with open(args.destination, 'w') as destination_file:
 		destination_file.write(args.content)
+
+	return 'saved to ' + args.destination
 
 def file_retrieve(args):
 	with open(args.destination, 'r') as destination_file:
@@ -44,70 +49,176 @@ def github_store(args):
 		commit_json['sha'] = response['sha']
 
 	# update/create a file
-	requests.put('https://{}:{}@api.github.com/repos/{}/{}/contents/{}'.format( \
+	response = requests.put('https://{}:{}@api.github.com/repos/{}/{}/contents/{}'.format( \
 		args.auth_user, args.auth_pass, args.repo_owner, args.repo, args.destination), data=json.dumps(commit_json), headers=headers).json()
+
+	# message is received if commit failed
+	if 'message' in response:
+		return response['message'].replace('\n', ' ')
+	
+	return 'successful commit to {}/{}'.format(args.repo_owner, args.repo)
 
 def github_retrieve(args):
 	return requests.get('https://{}:{}@raw.githubusercontent.com/{}/{}/master/{}'.format( \
 		args.auth_user, args.auth_pass, args.repo_owner, args.repo, args.destination)).text
+
+# ---------------------------------------------------------------------------------------------------
+
+def store(args):
+	print '{}:'.format(args.service),
+
+        try:
+                response = globals()[args.service + '_store'](args)
+                print 'successful' if response is True else response
+        except Exception as e:
+                print e
+
+def retrieve(args):
+	return globals()[args.service + '_retrieve'](args)
+
+def missing_args(args, service):
+	if service in list(service_args):
+		for attribute in service_args[service]:
+			if not attribute in args.__dict__ or not args.__dict__[attribute]:
+				return "{}: missing '{}' argument".format(service, attribute)
+		return None
+
+	return "Service '{}' is not supported".format(service)
+
+def parse_source(filepath):
+	args_list = []
+
+	with open(filepath, 'r') as sources:
+		line = sources.readline()
+ 
+		# split each line by space to get keyvalue strings
+		while line:
+			args = argparse.Namespace()
+			kvs = line.split(' ')
+
+			# split each element by '=' to separate key and value
+			for kv in kvs:
+				kv = kv.split('=')
+				args.__setattr__(kv[0], kv[1].strip())
+
+			args_list.append(args)
+			line = sources.readline()
+
+	return args_list
 
 def kiwi_main():
 	parser = argparse.ArgumentParser(description="work with common file storage services to store and retrieve files")
 	subparsers = parser.add_subparsers(title='actions', dest='action')
 	subparsers.required=True
 
+	# define subparsers
 	store_parser = subparsers.add_parser('store', help='store file within the storage service')
 	retrieve_parser = subparsers.add_parser('retrieve', help='retrieve file from the storage service', conflict_handler="resolve")
+	source_parser = subparsers.add_parser('source', help='use source file to store data within multiple services')
 	
-	# file contents
-	content_group = store_parser.add_mutually_exclusive_group(required=True)
-	content_group.add_argument('-c', '--content', help='file content', type=str)
-	content_group.add_argument('-f', '--file', help='source file to be stored', type=str)
-
+	# retrieve subparser specific arguments
 	retrieve_parser.add_argument('-f', '--file', help='local file destination', type=str)
 	retrieve_parser.add_argument('-h', '--hide', help='do not print out the retrieved data', action='store_true')
 
+	# source parser specific arguments
+	source_parser.add_argument('-S', '--source-file', help='destination to a file of sources', type=str, required=True)
+	source_parser.add_argument('-n', '--filename', help='filename within the destination folder', required=True)
+
 	for name, subparser in subparsers.choices.items():
 
-		# required arguments
-		subparser.add_argument('-s', '--service', help='storage service', type=str, required=True)
-		subparser.add_argument('-d', '--destination', help='file path within the storage', type=str, required=True)
+		# store / source common arguments
+		if name != 'retrieve':
 
-		# optional arguments
-		subparser.add_argument('-r', '--repo', help='repository name (when using Git)', type=str)
-		subparser.add_argument('-o', '--repo-owner', help='repository owner (when using Git)', type=str)
-		subparser.add_argument('-m', '--message', help='commit message (when using Git)', type=str)
-		subparser.add_argument('-u', '--auth-user', help='authentication username', type=str)
-		subparser.add_argument('-p', '--auth-pass', help='authentication password', type=str)
-		subparser.add_argument('-n', '--committer-user', help='name of the committer (when using Git)', type=str)
-		subparser.add_argument('-e', '--committer-email', help='e-mail of the committer (when using Git)', type=str)
+			# required arguments
+			content_group = subparser.add_mutually_exclusive_group(required=True)
+        		content_group.add_argument('-c', '--content', help='file content', type=str)
+        		content_group.add_argument('-f', '--file', help='file contents of which should be stored', type=str)
+
+			# optional arguments
+			subparser.add_argument('-m', '--message', help='commit message (when using Git)', type=str)
+			subparser.add_argument('--committer-user', help='name of the committer (when using Git)', type=str)
+                        subparser.add_argument('--committer-email', help='e-mail of the committer (when using Git)', type=str)
+
+		# store / retrieve common arguments
+		if name != 'source':
+
+			# required arguments
+			subparser.add_argument('-s', '--service', help='storage service', type=str, choices=service_args.keys(), required=True)
+			subparser.add_argument('-d', '--destination', help='file path within the storage', type=str, required=True)
+
+			# optional arguments
+			subparser.add_argument('-r', '--repo', help='repository name (when using Git)', type=str)
+			subparser.add_argument('-o', '--repo-owner', help='repository owner (when using Git)', type=str)
+			subparser.add_argument('-u', '--auth-user', help='authentication username', type=str)
+			subparser.add_argument('-p', '--auth-pass', help='authentication password', type=str)
 
 	args = parser.parse_args()
 
-	# make sure service is supported and has all the required parameters
-	if args.service in list(service_args):
-		for attribute, value in args.__dict__.iteritems():
-			if attribute in service_args[args.service] and not value:
-				print "{}: missing '{}' argument".format(args.service, attribute)
-				exit()
+	# source action
+	if args.action == 'source':
 
-		if args.action == 'store':
-			# store file contents in a variable
-			if args.file:
-				with open(args.file, 'r') as content_file:
-					args.content = content_file.read()
+		# parse source file, get list of namespaces
+		args_list = parse_source(args.source_file)
 
-			globals()[args.service + '_store'](args)
+		# for each source
+		for arg in args_list:
+
+			# check if any arguments are missing
+			missing = missing_args(arg, arg.service)
+
+			if not missing:
+
+				# set optional arguments
+				for attribute in store_parser._get_optional_actions():
+					if not arg.__contains__(attribute.dest):
+						if args.__contains__(attribute.dest):
+							arg.__setattr__(attribute.dest, args.__dict__[attribute.dest])
+						else:
+							arg.__setattr__(attribute.dest, None)
+
+				# read from file if filepath is given
+				if args.file:
+                        	        with open(args.file, 'r') as content_file:
+                                        	arg.content = content_file.read()
+				else:
+					arg.content = args.content
+
+				# set full destination
+				arg.destination = os.path.join(arg.destination, args.filename)
+
+				store(arg)
+			else:
+				print missing
+
+	# adhoc action
+	else:
+
+		# check if any arguments are missing
+		missing = missing_args(args, args.service)
+
+		if not missing:
+
+			# store action
+			if args.action == 'store':
+
+				# store file contents in a variable
+                        	if args.file:
+                                	with open(args.file, 'r') as content_file:
+                                        	args.content = content_file.read()
+
+                        	store(args)
+
+			# retrieve action
+			else:
+				data = retrieve(args)
+
+				if not args.hide:
+                                	print data
+
+				# store data in a file
+                        	if args.file:
+                                	with open(args.file, 'w') as content_file:
+                                        	content_file.write(data)
 
 		else:
-			data = globals()[args.service + '_retrieve'](args)
-
-			if not args.hide:
-				print data
-
-			# store data in a file
-			if args.file:
-				with open(args.file, 'w') as content_file:
-					content_file.write(data)
-	else:
-		print "Service '{}' is not supported".format(args.service)	
+			print missing
