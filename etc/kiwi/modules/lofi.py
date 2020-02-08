ceddaf0af149579d0acd96667686f69240ad848e
isLonerism <kiwiclitool@gmail.com>
2020-02-08 11:35:18 +0200

    feat(lofi api query, choses from top 3 streams)

--- etc/kiwi/modules/lofi.py
+++ etc/kiwi/modules/lofi.py
@@ -3,8 +3,15 @@ kiwi_description = 'Open a new browser window with a lofi music stream'
 
 import distutils.spawn
 import subprocess
+from requests import get
+from random import randint
 
-lofi_url = "https://www.youtube.com/watch?v=hHW1oY26kxQ"
+# pre generated key configured to only query the YouTube Data API (from home)
+key = 'AIzaSyCFbq79LHDA2eoOeb_BPjWZSjQOCYFw6K0'
+videoId = get('https://www.googleapis.com/youtube/v3/search?part={}&type={}&eventType={}&order={}&q={}&maxResults={}&key={}'.format(
+	'snippet', 'video', 'live', 'viewCount', 'lofi', '3', key
+)).json()['items'][randint(0, 2)]['id']['videoId']
+lofi_url = "https://www.youtube.com/watch?v=" + str(videoId)
 
 def kiwi_main():
 	if distutils.spawn.find_executable("explorer.exe") is not None:
