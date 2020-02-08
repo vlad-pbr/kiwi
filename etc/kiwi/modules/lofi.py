a5549f33df81bc661da6514b164814578e5ba42b
isLonerism <kiwiclitool@gmail.com>
2020-02-08 18:05:37 +0200

    feat(parse_config function, lofi config file)

--- etc/kiwi/modules/lofi.py
+++ etc/kiwi/modules/lofi.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python2
 kiwi_description = 'Open a new browser window with a lofi music stream'
 
-import distutils.spawn
-import subprocess
+from subprocess import call
+from distutils.spawn import find_executable
+from os.path import isfile
 from requests import get
 from random import randint
 
-# pre generated key configured to only query the YouTube Data API (from home)
-key = 'AIzaSyCFbq79LHDA2eoOeb_BPjWZSjQOCYFw6K0'
-videoId = get('https://www.googleapis.com/youtube/v3/search?part={}&type={}&eventType={}&order={}&q={}&maxResults={}&key={}'.format(
-	'snippet', 'video', 'live', 'viewCount', 'lofi', '3', key
-)).json()['items'][randint(0, 2)]['id']['videoId']
-lofi_url = "https://www.youtube.com/watch?v=" + str(videoId)
+def get_url(kiwi):
+	videoId = 'hHW1oY26kxQ'
 
-def kiwi_main():
-	if distutils.spawn.find_executable("explorer.exe") is not None:
-		subprocess.call(["explorer.exe", lofi_url + "&\""])
-	elif distutils.spawn.find_executable("xdg-open") is not None:
-		subprocess.call(["xdg-open", lofi_url])
+	if isfile(kiwi['module_home'] + kiwi['module_name'] + '.conf'):
+		config_file = kiwi['parse_config'](kiwi['module_home'] + kiwi['module_name'] + '.conf')
+		link = config_file.get('link')
+		key = config_file.get('key')
+
+		# link has priority
+		if link:
+			return link
+		if key:
+			videoId = get('https://www.googleapis.com/youtube/v3/search?part={}&type={}&eventType={}&order={}&q={}&maxResults={}&key={}'.format(
+				'snippet', 'video', 'live', 'viewCount', 'lofi', '3', key
+			)).json()['items'][randint(0, 2)]['id']['videoId']
+
+	return 'https://www.youtube.com/watch?v=' + videoId
+
+def kiwi_main(kiwi):
+	if find_executable("explorer.exe") is not None:
+		call(["explorer.exe", get_url(kiwi) + "&\""])
+	elif find_executable("xdg-open") is not None:
+		call(["xdg-open", get_url(kiwi)])
 	else:
 		print "No available browsers found."
