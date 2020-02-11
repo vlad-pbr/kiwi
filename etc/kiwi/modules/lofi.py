99134970ce00af5e83a4f7600fd3466206f9e6a2
isLonerism <kiwiclitool@gmail.com>
2020-02-11 23:21:24 +0200

    feat(lofi help flag)

--- etc/kiwi/modules/lofi.py
+++ etc/kiwi/modules/lofi.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python2
 
+"""
+Run lofi without any arguments to open a hardcoded lofi stream link.
+
+Lofi will try to read a key=value file located at ~/.kiwi/lofi/lofi.conf. It's gonna look for the following keys:
+
+* link: custom URL to a lofi stream to open
+* key: YouTube Data v3 API key to query the YouTube search API and get the top 3 lofi streams
+
+In case both are specified, 'link' takes priority.
+"""
+
 from subprocess import call
 from distutils.spawn import find_executable
 from os.path import isfile
 from requests import get
 from random import randint
+from sys import argv
 
 def get_url(kiwi):
 	videoId = 'hHW1oY26kxQ'
@@ -28,7 +40,13 @@ def kiwi_main(kiwi):
 
 	"""Open a new browser window with a lofi music stream"""
 
-	if find_executable("explorer.exe") is not None:
+	if len(argv) > 1:
+		if argv[1] == '--help' or argv[1] == '-h':
+			print __doc__
+		else:
+			print 'lofi: use --help'
+
+	elif find_executable("explorer.exe") is not None:
 		call(["explorer.exe", get_url(kiwi) + "&\""])
 	elif find_executable("xdg-open") is not None:
 		call(["xdg-open", get_url(kiwi)])
