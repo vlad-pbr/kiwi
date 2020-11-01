b8ce8ef86c28aed483f6388fb0c3f8c0cd9198f7
Vlad Poberezhny <vlad.pbr@gmail.com>
2020-11-01 23:09:02 +0200

    feat(basic kiwi server)

--- runtime/server.py
+++ runtime/server.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+from flask import Flask, request
+
+app = Flask(__name__[:-3], static_url_path="/modules")
+
+@app.route('/module/<path:module>')
+def module(module):
+	return "Path: " + module + " | Args: " + str(request.args)
+
 def run(kiwi):
-	kiwi.say("Work in progress...")
+
+	# server modules as static files
+	app.static_folder = kiwi.Config.kiwi_local_modules_dir
+
+	app.run(host="0.0.0.0")
