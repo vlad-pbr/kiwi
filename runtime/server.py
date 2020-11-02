a700103961b98cc6bcd6c867038dba35028e8602
Vlad Poberezhny <vlad.pbr@gmail.com>
2020-11-02 23:28:45 +0200

    feat(server port parameter)

--- runtime/server.py
+++ runtime/server.py
@@ -94,5 +94,5 @@ def run(_kiwi):
 		"runtime": runtime_asset
 	}
 
-	http_server = WSGIServer(('', 5000), app)
+	http_server = WSGIServer(('', int(kiwi.Config.server_port)), app)
 	http_server.serve_forever()
