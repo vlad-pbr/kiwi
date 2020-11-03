0a2b0702d261273fb2e0b62d73f089bf2c2fbbbc
Vlad Poberezhny <vlad.pbr@gmail.com>
2020-11-03 21:07:02 +0200

    feat(server daemon toggle)

--- runtime/server.py
+++ runtime/server.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-from os import listdir
-from os.path import join, isdir, exists
+from os import listdir, kill, remove
+from os.path import join, isdir, exists, isfile
 from json import dumps
+from multiprocessing import Process
+from daemonize import Daemonize
 
 from flask import Flask, request, abort, send_from_directory
 from werkzeug.exceptions import HTTPException
@@ -76,6 +78,28 @@ def serve_asset(asset, path):
 
 def run(_kiwi):
 
+	pid_file_path = join(_kiwi.Config.home_dir, 'PID')
+
+	# open PID file
+	if isfile(pid_file_path):
+		with open(pid_file_path, 'r') as pid_file:
+			pid = int(pid_file.read())
+
+			# if server is already running
+			if pid:
+				try:
+					kill(pid, 0)
+				except OSError:
+					pass
+				else:
+					_kiwi.say('stopping server...')
+
+					# kill daemon and remove PID file
+					kill(pid, 9)
+					remove(pid_file_path)
+
+					exit(0)
+
 	global kiwi
 	global api
 	global assets
@@ -94,5 +118,9 @@ def run(_kiwi):
 		"runtime": runtime_asset
 	}
 
-	http_server = WSGIServer(('', int(kiwi.Config.server_port)), app)
-	http_server.serve_forever()
+	_kiwi.say('starting server...')
+
+	# toggle server process
+	http_server = WSGIServer(('', int(_kiwi.Config.server_port)), app)
+	server_daemon = Daemonize(app=app.import_name, pid=pid_file_path, action=http_server.serve_forever)
+	server_daemon.start()
